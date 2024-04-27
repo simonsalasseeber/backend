fc575b4d6778a82e509df4ed1cbe0cda117df917
simonsalasseeber@gmail.com <simonsalasseeber@gmail.com>
2024-04-27 23:55:38 +0200

    lok

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,6 @@ FROM node
 WORKDIR /app
 COPY package*.json ./
 RUN npm install
-RUN npm run migration:run
 COPY . .
 EXPOSE 3000
 CMD ["npm", "run", "start"]
--- src/config/typeorm.ts
+++ src/config/typeorm.ts
@@ -4,37 +4,25 @@ import { registerAs } from "@nestjs/config";
 
 dotenvConfig({path: '.development.env'});
 
-const {
-    DB_HOST,
-    DB_PORT,
-    DB_USERNAME,
-    DB_PASSWORD,
-    DB_NAME,
-   } = process.env;
-
-   const url = require('url');
+// const {
+//     DB_HOST,
+//     DB_PORT,
+//     DB_USERNAME,
+//     DB_PASSWORD,
+//     DB_NAME,
+//    } = process.env;
 
 // Suponiendo que DB_URL es tu variable de entorno con la URL de la base de datos
 const dbUrl = process.env.DB_URL;
 
-// Analiza la URL de la base de datos
+const url = require('url');
 const parsedUrl = url.parse(dbUrl);
-
-// Extrae los componentes de la URL
-const host = parsedUrl.hostname;
-const deployedport = parsedUrl.port || 3000; 
-const username = parsedUrl.auth.split(':')[0];
-const password = parsedUrl.auth.split(':')[1];
-const database = parsedUrl.pathname.substring(1);
-
+const port = parsedUrl.port || 3000; 
 
 const config = {
  type: 'postgres',
- host: host, // change for 'postgresdb'
- port: deployedport,
- username: username,
- password: password,
- database: database,
+ url: dbUrl,
+ port: port,
  entities: ['dist/**/*.entity{.ts,.js}'],
  migrations: ['dist/migrations/*{.ts,.js}'],
  synchronize: false,
